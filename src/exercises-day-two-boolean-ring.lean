f246571e8961fe8b185c18f68c0cc5099f22e199
Jesse Michael Han <hyrodi@gmail.com>
2019-06-18 17:21:56 +0700

    Update boolean_ring

--- src/exercises-day-two-boolean-ring.lean
+++ src/exercises-day-two-boolean-ring.lean
@@ -3,6 +3,7 @@ import order.boolean_algebra
 open lattice
 
 /-
+EXERCISE:
 
 In mathematics, a Boolean ring R is a ring for which x² = x for all x in R.
 
@@ -11,6 +12,8 @@ Define a class of boolean rings. It should extend the typeclass of rings.
 -/
 
 class boolean_ring (α : Type*) := sorry
+
+open boolean_ring
 
 
 universe u
@@ -32,13 +35,26 @@ Given a Boolean ring R, for x and y in R we can define
 These operations then satisfy all of the axioms for meets, joins, and complements in a Boolean algebra. Thus every Boolean ring becomes a Boolean algebra. 
 -/
 
-/- According to the above, we define custom notation for addition in α -/
 local notation x ` ⊕ ` y:= (x : α) + (y : α)
 
-/- Given the boolean_ring instance on α, construct the boolean_algebra instance on α. -/
+/- Every Boolean ring R satisfies x ⊕ x = 0 for all x in R, because we know
+
+    x ⊕ x = (x ⊕ x)² = x² ⊕ x² ⊕ x² ⊕ x² = x ⊕ x ⊕ x ⊕ x
+
+and since (R,⊕) is an abelian group, we can subtract x ⊕ x from both sides of this equation, which gives x ⊕ x = 0. A similar proof shows that every Boolean ring is commutative:
+
+    x ⊕ y = (x ⊕ y)² = x² ⊕ xy ⊕ yx ⊕ y² = x ⊕ xy ⊕ yx ⊕ y
+
+ and this yields xy ⊕ yx = 0, which means xy = yx (using the first property above). -/
+
+/- EXERCISE: Prove a lemma which says that ∀ x : α, x ⊕ x = 0.-/
+
+/- According to the above, we define custom notation for addition in α -/
+
+
+/- EXERCISE: Given the boolean_ring instance on α, construct the boolean_algebra instance on α. -/
 
 /- the inf operation should be x * y, the sup operation should be x ⊕ y ⊕ x * y, and the complement operation should be 1 ⊕ x. -/
 
 instance boolean_algebra_of_boolean_ring {α : Type*} [boolean_ring α] : boolean_algebra α :=
 {!_!} -- use the hole command for creating a structure stub
-
